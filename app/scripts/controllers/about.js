11e7b7809e380ee1d6098fd0cff3bb1e5f844008
IGerrebrands <igerrebrands@competa.com>
2016-06-12 03:51:31 +0200

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name overwatchApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the overwatchApp
+ */
+angular.module('overwatchApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name overwatchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the overwatchApp
+ */
+angular.module('overwatchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
